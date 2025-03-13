ff46be87d89a0511bbc19cd67524e381d113d739
djilali <derndjilali@gmail.com>
2025-03-13 22:44:54 +0100

    add visa controller

---
+++ app/Http/Controllers/API/VisaController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Destination;
+use App\Models\VisaType;
+
+class VisaController extends Controller
+{
+    public function destinations()
+    {
+        $destinations = Destination::select('id', 'name')->get();
+        return response()->json(["data" => $destinations]);
+    }
+
+    public function visasTypes($id)
+    {
+        $visasTypes = Destination::find($id)->visas;
+        return response()->json(["data" => $visasTypes]);
+    }
+
+    public function PassengersVisasInfo(Request $request)
+    {
+        $visa_type_id = $request->visa_type_id;
+        $adults = $request->adults ?? 0;
+        $children = $request->children ?? 0;
+
+        $visa = VisaType::find($visa_type_id);
+
+        if ($visa) {
+            return response()->json([
+                "data" => [
+                    "adult" => [
+                        "unit_price" => $visa->adult_price,
+                        "count" => $adults,
+                        "documents" => $visa->documents ?? [], 
+                    ],
+                    "child" => [
+                        "unit_price" => $visa->child_price,
+                        "count" => $children,
+                        "documents" => $visa->documents ?? [], 
+                    ],
+                    "total_price" => ($adults * $visa->adult_price) + ($children * $visa->child_price)
+                ]
+            ]);
+        }
+
+        return response()->json(["data" => null], 404);
+    }
+
+    public function totalVisaPrice(Request $request)
+    {
+        $total = 0;
+        $visa_type_id = $request->visa_type_id;
+        $adults = $request->adults;
+        $children = $request->children;
+        $visa = VisaType::find($visa_type_id);
+
+        if ($visa) {
+            $total = $adults * $visa->adult_price + $children * $visa->child_price;
+        }
+
+        return response()->json(["data" => ["adults" => $adults, "children" => $children, "total" => $total]]);
+    }
+    
+}
