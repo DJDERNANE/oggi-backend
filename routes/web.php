7ce8be44db4353d0890f485a24aad7846d013586
djilali <derndjilali@gmail.com>
2025-05-21 23:35:33 +0100

    test preview image

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VisaApplicationController;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return redirect('/admin');
@@ -12,6 +13,17 @@ Route::get('/storage/link', function (){
     $linkFolder = '/home/oggitrav/oggi-panel/public/storage';
     symlink($targetFolder,$linkFolder);
     echo 'Symlink process successfully completed';
+});
+
+
+Route::get('/file-preview', function () {
+    $path = 'visas/01JVTF6KN6SWA91SXQQA60G5MQ.pdf';
+
+    if (!Storage::disk('public')->exists($path)) {
+        abort(404);
+    }
+
+    return response()->file(storage_path("app/public/{$path}"));
 });
 
 Route::get('/login', function () {
